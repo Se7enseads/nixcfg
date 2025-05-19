f770030adc86383076f103a807a55301b0c4c410
Tiramisu <135003824+Se7enseads@users.noreply.github.com>
2025-05-19 22:25:28 +0300

    :wrench: add `swap` to fix memory shortage

--- hosts/okashi/disko.nix
+++ hosts/okashi/disko.nix
@@ -21,6 +21,23 @@
                 mountpoint = "/boot";
               };
             };
+            encryptedSwap = {
+              size = "10M";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority =
+                  100; # prefer to encrypt as long as we have space for it
+              };
+            };
+            plainSwap = {
+              size = "10G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
             root = {
               size = "100%";
               content = {
