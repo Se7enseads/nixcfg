cc29e26227e14ee57c3e95f32a386844a9d4e6f7
Tiramisu <135003824+Se7enseads@users.noreply.github.com>
2025-06-15 02:40:56 +0300

    :sparkles: add `firewall`
    - Replace nm with iwd

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -1,1 +1,13 @@
-{ networking = { networkmanager.enable = true; }; }
+{
+  networking = {
+    networkmanager.enable = true;
+    wireless.iwd.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 443 80 ];
+      allowedUDPPorts = [ 443 80 44857 ];
+      allowPing = false;
+    };
+  };
+
+}
