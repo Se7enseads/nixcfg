87b8b1076fc112ec31482aa3044c63ec7b00aa55
Tiramisu <135003824+Se7enseads@users.noreply.github.com>
2025-06-15 02:42:16 +0300

    :sparkles: add `security`
    - Replace `sudo` with `doas`

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -6,6 +6,7 @@
     ./boot.nix
     ./networking.nix
     ./programs.nix
+    ./security.nix
     ./users
   ];
 
---
+++ hosts/common/security.nix
@@ -0,0 +1,18 @@
+{ username, ... }:
+
+{
+  security = {
+    sudo.enable = false;
+    doas = {
+      enable = true;
+      extraRules = [{
+        users = [ "${username}" ];
+        keepEnv = true;
+        persist = true;
+      }];
+    };
+
+    # Extra security
+    protectKernelImage = true;
+  };
+}
